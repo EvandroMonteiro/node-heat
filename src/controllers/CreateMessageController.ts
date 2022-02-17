34d607e4c74ecd4193e55b30377c2e5563090bce
Evandro Monteiro <goodvandro@gmail.com>
2022-02-17 08:37:21 +0000

    initial commit

---
+++ src/controllers/CreateMessageController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { CreateMessageService } from "../services/CreateMessageService";
+
+class CreateMessageController {
+  async handle(request: Request, response: Response) {
+    const { message } = request.body
+    const { user_id } = request
+
+    console.log('aqui')
+
+    const service = new CreateMessageService()
+
+    const result = await service.execute(message, user_id)
+
+    return response.json(result)
+  }
+}
+
+export { CreateMessageController }
